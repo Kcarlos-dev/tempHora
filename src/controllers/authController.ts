08ca94c619d5a7171b4249f3c450c61962d6af7b
kcarlos-dev <ca850075@gmail.com>
2026-04-16 20:00:17 -0300

    teste doido

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import config from '../config/index';
 import authService from '../services/authService';
 
 export async function login(req: Request, res: Response, next: NextFunction) {
@@ -6,7 +7,7 @@ export async function login(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'E-mail e senha são obrigatórios.' });
+      return res.status(400).json({ message: 'E-mail e senha são obrigatórios.',data:config.mysql.password});
     }
 
     const token = await authService.login(email, password);
