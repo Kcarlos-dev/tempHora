4eb8606b72c6e14a89805101889be99203a4245e
kcarlos-dev <ca850075@gmail.com>
2026-04-17 12:06:23 -0300

    corrigindo rotas de atestado

--- src/routes/atestado.ts
+++ src/routes/atestado.ts
@@ -14,7 +14,7 @@ const router = Router();
 
 //router.get('/', listAtestados);
 router.get('/:id_empresa/cpf/:cpf',authMiddleware, getAtestadoByCpfColaborador);
-router.post('/:id_empresa:id_colaborador',authMiddleware, roleMiddleware(['admin', 'root', 'rh', 'colaborador']), uploadAtestado.single('arquivo'), createAtestado);
+router.post('/:id_empresa/:id_colaborador',authMiddleware, roleMiddleware(['admin', 'root', 'rh', 'colaborador']), uploadAtestado.single('arquivo'), createAtestado);
 router.put('/:id_empresa/:id',authMiddleware, roleMiddleware(['admin', 'root', 'rh']), updateAtestado);
 router.delete('/:id_empresa/:id',authMiddleware, roleMiddleware(['admin', 'root']), deleteAtestado);
 
